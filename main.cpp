70cdcf98c7e683e38d02fa419427f2a05ac207b4
awesomearjun <64511698+awesomearjun@users.noreply.github.com>
2022-11-20 19:19:01 -0500

    Add comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+/* Imports */
+
 #include <iostream>
 #include <fstream>
 
@@ -6,15 +8,24 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+/* Jump to main function */
+
+/* params: loginMode (register or login */
 void login(const char loginMode) {
+	/* Switch for loginMode = 1 or loginMode = 2 */
 	switch(loginMode) {
+		/* loginMode = 1? */
 		case '1':
 			{
+				/* databases in append mode */
 				std::ofstream nameDB("names.txt", std::ios_base::app);
 				std::ofstream passDB("pass.txt", std::ios_base::app);
+				
+				/* username & password */
 				string username;
 				string password;
 
+				/* Add account */
 				cout << "Register" << endl;
 
 				cout << "Enter a username: " << endl;
@@ -22,20 +33,31 @@ void login(const char loginMode) {
 				cout << "Enter a password: " << endl;
 				cin >> password;	
 
+				/* Write to DBs */
 				nameDB << username << endl;
 				passDB << password << endl;
 			}
-			break;	
+			break;
+			
+		/* loginMode = 2? */
 		case '2':
 			{
+				/* DBs */
 				std::ifstream nameDB("names.txt");
 				std::ifstream passDB("pass.txt");
+				
+				/* user input username & password */
 				string uiUsername;
 				string uiPassword;
+				
+				/* field input username & password */
 				string fUsername;
 				string fPassword;
+				
+				/* bools for usernameCorrect and passwordCorrect */
 				bool usernameCorrect, passwordCorrect;
 
+				/* Login */
 				cout << "Login" << endl;
 
 				cout << "Enter your username: " << endl;
@@ -43,7 +65,9 @@ void login(const char loginMode) {
 				cout << "Enter your password: " << endl;
 				cin >> uiPassword;
 
+				/* Loop over usernames and set username to fUsername */
 				while(std::getline(nameDB, fUsername)) {
+					/* Sets username to correct/incorrect */
 					if(uiUsername == fUsername) {
 						usernameCorrect = true;	
 						break;
@@ -52,7 +76,7 @@ void login(const char loginMode) {
 					}
 				}
 
-				
+				/* Same here */
 				while(std::getline(passDB, fPassword)) {
 					if(uiPassword == fPassword) {
 						passwordCorrect = true;	
@@ -62,6 +86,10 @@ void login(const char loginMode) {
 					}
 				}
 
+				/* 
+				If username and password are correct, say hi to user,
+				else, no.
+				*/
 				if(usernameCorrect && passwordCorrect) {
 					cout << "Hello, " << uiUsername << "!" << endl;	
 				} else  {
@@ -73,13 +101,21 @@ void login(const char loginMode) {
 }
 
 int main() {
+	/* New variable mode */
 	char mode; 
+	
+	/* Introduction */
 	cout << "Hi! Welcome to my login system!" << endl;
 	cout << "Enter a mode: " << endl;
 	cout << "1 = Register" << endl << "2 = Login" << endl;
+	
+	/* Set mode to user input */
 	cin >> mode;
 
+	/* Calls login function */
 	login(mode);
+	
+	/* Jump to login function up */
 	
 	return 0;
 }
